746d137eb105ff09411bb9d02d2e70451d63f09b
Anthony Lopez <grungekarma@gmail.com>
2021-04-20 20:04:51 -0500

    VariablesAndPrimatives.2

---
+++ classActivities/2.2.2-JSVariablesAndPrimitives.js
@@ -0,0 +1,59 @@
+// create a variable called "name" that references a string
+
+const name = "Anthony";
+
+// create a variable called "favoriteSong" that references a string
+
+let favoriteSong = "Look but Dont Touch";
+
+// create a variable called "wage" that references a number
+
+let wage = 0;
+
+// create a variable called "age" that references a number
+
+let age = 30;
+
+// create a variable called "onlyChild" that references a boolean
+
+let onlyChild = false;
+
+// create a variable called "satisfied" that references a boolean
+
+let satisfied = true;
+
+// create a variable called "favoriteThing" that references ANY primitive value
+// print the data type of "favoriteThing"
+
+let favoriteThing = "Music";
+console.log(favoriteThing);
+
+// ! You heard a new song that became your favorite.
+// Reassign the variable "favoriteSong"
+
+favoriteSong = "G.O.A.T";
+
+
+// ! You no longer like the type of data stored in "favoriteThing"
+// Reassign "favoriteThing" to a different primitive data type and print its type
+
+favoriteThing = 7;
+
+// ! Happy Birthday!
+// Reassign "age" to a new value
+
+age = 31;
+
+// ! You got a big raise at work.
+// Reassign "wage" to a new value
+
+wage = 50,000;
+
+// ! Your raise changed your job satisfaction.
+// Assign "satisfied" to true
+
+
+// use "console.log()" and "+" to print a sentence that includes two variables
+console.log(
+  "Hello my name is " + name+ " and my favorite song is " + favoriteSong +"."
+)
