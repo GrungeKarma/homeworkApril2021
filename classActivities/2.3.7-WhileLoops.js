825c68ff490df402128c78d4f92383e8e248d98b
Anthony Lopez <grungekarma@gmail.com>
2021-04-21 21:48:09 -0500

    4/21/21 coursework

---
+++ classActivities/2.3.7-WhileLoops.js
@@ -0,0 +1,22 @@
+//  TO DO: create a while loop that TOTALS the numbers from 1 to 100
+// create a variable to represent the current number
+
+let num = 1;
+let total = 0;
+while (num <= 100){
+  total += num;
+  num++;
+}
+console.log(total);
+
+
+// create a variable to represent the current total
+
+// write a while loop that sums the numbers from 1 to 100
+
+//  TO DO: create a while loop that TOTALS the numbers from 1 to 100
+// create a variable to represent the current number
+
+// create a variable to represent the current total
+
+// write a while loop that sums the numbers from 1 to 100
