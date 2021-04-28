beed04e015d0d547b87e65995e772ad8a9fb99b0
Anthony Lopez <grungekarma@gmail.com>
2021-04-27 19:13:25 -0500

    first activity

---
+++ classActivities/3.2.1-Arrays.js
@@ -0,0 +1,19 @@
+// create an Array using an Array literal
+//let stores = ["Target", "Ikea", "Costco", "Walmart", "Walgreens", "Price Chopper", "Hyvee"]
+// access the 1st item in the Array
+//console.log(stores[0]);
+// access the last item in the Array
+//console.log(stores[6]);
+// print the length of the Array
+//console.log(stores.length);
+// use the length property to access the last item in the Array
+//console.log(stores.length -1);
+//console.log(stores[7]);
+// with for...of, loop over the Array, modify the value and add to a different Array
+numbers = [5, 6, 2,];
+for (number of numbers){
+  console.log(number + 1);
+  console.log(number);
+}
+
+
