741aa388d68ee3ebeded3c83caca1dcd638047d6
Anthony Lopez <grungekarma@gmail.com>
2021-04-29 02:53:07 -0500

    oods and ends

--- homework/homeworkWeek2.js
+++ homework/homeworkWeek2.js
@@ -1,26 +1,20 @@
 const pizzaPlace = "Tony's Pizza";
 
 let numberOfToppings = 10;
+let orderOfToppings = 10;
 
-console.log(pizzaPlace, numberOfToppings);
+console.log(`Here at ${pizzaPlace} we offer ${numberOfToppings} toppings!`);
 
-console.log (`Here at ${pizzaPlace} we offer ${numberOfToppings} toppings!`);
-
-if (numberOfToppings <= 9) {
-  console.log('Quality, not quantity!');
+if (orderOfToppings >= 9) {
+  console.log(`Quality`);
+} else if (orderOfToppings >= 0) {
+  console.log(`We do not serve fools!`);
+} else {
+  console.log(`A whole lot of pizza!`);
 }
-  else {
-    console.log (`A whole lot of pizza!`);
+
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
   }
-
-  for (let i = 1; i <= 10; i++){
-    if (i%2 === 0){
-      console.log(i);
-    }
-
-  }
-
-
-
-
-
+}
