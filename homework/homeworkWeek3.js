23a7690daf26216c7e9ec7ff52f5f5868daafd76
Anthony Lopez <grungekarma@gmail.com>
2021-05-01 01:55:06 -0500

    Mostly done before bonus challenge

--- homework/homeworkWeek3.js
+++ homework/homeworkWeek3.js
@@ -1,8 +1,44 @@
+/* eslint-disable for-direction */
 /* eslint-disable no-undef */
 
-let pizzaToppings = ["pepperoni", "sausage", "ham", "peppers", "feta", "bacon"];
-//array of toppings
-greetCustomers = name => {
-  return `Hello ${name}, we have ${pizzaToppings[0]}, ${pizzaToppings[1]}, ${pizzaToppings[2]}, ${pizzaToppings[3]}, ${pizzaToppings[4]}, and ${pizzaToppings[5]} available.`;
+let pizzaToppings = [
+  " pepperoni",
+  " sausage",
+  " ham",
+  " peppers",
+  " feta",
+  " bacon"
+];
+
+greetCustomer = name => {
+  for (let i = 0; i < pizzaToppings.length; i++) {
+    //iterate through toppings
+    console.log(`Welcome to Tony's Pizza House ${name}, our toppings are:`);
+    let listToppings = pizzaToppings.toString();
+    console.log(listToppings);
+    return pizzaToppings[i];
+  }
 };
-console.log(greetCustomers("Jerry"));
+
+
+getPizzaOrder = (size, crust, ...toppings) => {
+  console.log(
+    `One ${size}${crust}crust pizza with ${toppings} coming right up!.`
+  );
+  return (toppingArr = [size, crust, toppings]);
+};
+
+getPizzaOrder("large ", "thin ", "pepperoni ", "sausage ");
+
+preparePizza = toppingArr => {
+  console.log(`###Cooking Pizza###`);
+  let order ={
+    size: toppingArr[0],
+    crust: toppingArr[1],
+    toppings: toppingArr[2]
+  };
+  return (pizza = order);
+};
+preparePizza(toppingArr);
+
+console.log(pizza);
