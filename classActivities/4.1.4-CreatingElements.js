0d07ce77f56c7d3c25e162a5280f154d6ed4f2f5
Anthony Lopez <grungekarma@gmail.com>
2021-05-03 21:26:02 -0500

    fourth activity

---
+++ classActivities/4.1.4-CreatingElements.js
@@ -0,0 +1,10 @@
+// create a new unordered list (ul) element
+const newUl = document.createElement("ul");
+
+// remove the paragraph element in the nav-bar
+document.querySelector(".nav-bar").remove("p");
+// add your new ul element to the nav-bar
+
+// create two new list item (li) elements, and add some text to them
+
+// add the li elements to the ul in the nav-bar
