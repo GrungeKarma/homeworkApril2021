3d6184272eae0ee3a03a0c983265bee890fd2eda
Anthony Lopez <grungekarma@gmail.com>
2021-05-06 22:32:10 -0500

    start homework

---
+++ homework/week4Homework/index.js
@@ -0,0 +1,3 @@
+/*GOAL: Create a website that informs a visitor about something. The website should be readable and look nice: the website's basic appearance matters more than the particular content.
+
+You could create the beginnings of a professional portfolio website highlighting your career path and skills, or you could create a website that simply displays information about something you are interested in.*/
